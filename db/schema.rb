ea6684eeea929b97e2164961e114c0505e16f8cb
Josh Kleinpeter <josh@kleinpeter.org>
2011-11-05 14:09:49 -0500

    added name to user

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20111105185709) do
   end
 
   create_table "users", :force => true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
